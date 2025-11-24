7445f61c9d07e3adbf73407a7df7fdda4312c362
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-11-24 20:09:11 +0530

    github action

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/hasii-music-site/",
+  base: '/hasii-music-site/', // Set the base path for deploying to GitHub Pages
   build: {
     outDir: "dist",
     assetsDir: "assets",
