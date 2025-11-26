e23b011534713d9138610b7bed22d92e17b9bd9b
Hasindu Nagolla <116471812+hasindu-nagolla@users.noreply.github.com>
2025-11-26 09:41:19 +0530

    dns change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hasii-music-site/", // Set the base path for deploying to GitHub Pages
+  base: "/", // Use root path for custom domain
   build: {
     outDir: "dist",
     assetsDir: "assets",
